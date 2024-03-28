064d1634a55fbec00fce351ad6afbbf293fa98f3
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-03-28 21:38:05 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/get-ip", (req, res) => {
+  const ipAddress = req.ip;
+  res.json({ ip: ipAddress });
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
